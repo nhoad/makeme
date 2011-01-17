4999c051772ac5b8f6ddfe289ddecff4b2eaa972
Nathan Hoad <nathan@getoffmalawn.com>
2011-01-17 22:45:14 +1000

    Made some more updates to the torrent script.
    The torrent script now supports turning Turtle mode on and off, with turtle mode on supporting that you specify a download limit.

--- scripts/torrent.py
+++ scripts/torrent.py
@@ -4,24 +4,12 @@ import errno
 import urllib
 import re
 import transmissionrpc
-from threading import Thread
 
 add_pattern = r'ADD (\S+)'
 add_stop_pattern = r'ADD-STOP (\S+)'
 server_pattern = r'SERVER (\S+)'
-
-port = 9091
-server = 'localhost'
-should_stop = False
-
-result = re.search(server_pattern, sys.argv[4])
-
-if result is not None:
-    if len(result.groups()) == 1:
-        server = result.groups()[0]
-    else:
-        server = result.groups()[0]
-
+port_pattern = r'PORT (\S+)'
+turtle_pattern = r'TURTLE-ON (\S+)'
 
 def add_torrents(client):
     """Add all torrents to the server"""
@@ -34,11 +22,16 @@ def add_torrents(client):
 
     for t in torrents:
         id = client.add_url(t)
+        print "%s added" % t
 
         client.stop(id)
 
+
 def show_progress(client):
     """Display progress of all current torrents"""
+    if sys.argv[4].find('PROGRESS') == -1:
+        return
+
     torrents = client.list()
 
     for i in torrents.keys():
@@ -46,11 +39,13 @@ def show_progress(client):
         percent = torrents[i].progress
         print "%d - %s - %.2f%%" % (i, name, percent)
 
+
 def magic_torrents(client, command):
     ids = []
     text = sys.argv[4]
 
     b = text.find(command.upper())
+
     if b == -1:
         return
 
@@ -67,11 +62,54 @@ def magic_torrents(client, command):
     for i in ids:
         getattr(client, command)(i)
 
+    print "All torrents have been %s'ed" % command
+
+
+def turtle_on(client):
+    result = re.search(turtle_pattern, sys.argv[4])
+
+    if result:
+        limit = int(result.groups()[0])
+
+        client.set_session(alt_speed_enabled=True)
+        print "TURTLE MODE TURNED ON"
+        client.set_session(alt_speed_down=limit)
+        print "SPEED LIMITED TO %d KiB/s" % limit
+
+
+def turtle_off(client):
+    if sys.argv[4].find("TURTLE-OFF") != -1:
+        client.set_session(alt_speed_enabled=False)
+        print "TURTLE MODE TURNED OFF"
+
+
 def start_torrents(client):
     """start all or some of the torrents using START command"""
     magic_torrents(client, 'start')
 
+
 def stop_torrents(client):
     magic_torrents(client, 'stop')
 
+
+port = 9091
+server = 'localhost'
+
+result = re.search(server_pattern, sys.argv[4])
+
+if result:
+    server = result.groups()[0]
+
+result = re.search(port_pattern, sys.argv[4])
+
+if result:
+    port = int(result.groups()[0])
+
 tc = transmissionrpc.Client(server, port=port)
+
+add_torrents(tc)
+show_progress(tc)
+start_torrents(tc)
+stop_torrents(tc)
+turtle_on(tc)
+turtle_off(tc)
