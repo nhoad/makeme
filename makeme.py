2aa8b6aff1e81330edd651155853fd80f869dbfe
Nathan Hoad <nathan@getoffmalawn.com>
2011-07-05 21:44:24 +1000

    Removed MakeMe's inheritance from dict, and added docstrings.
    inheriting from dict for configuration is just lazy and stupid.

--- makeme.py
+++ makeme.py
@@ -1,12 +1,14 @@
 import os
-class MakeMe(dict):
+class MakeMe(object):
     def __init__(self):
-        dict.__init__(self)
+        """Initialise the MakeMe object. Sets _running to True, loads the
+        config and logging, forking if requested.
 
+        """
         self._running = True
-        #self._load_config()
+        self._load_config()
 
-        if self['fork']:
+        if self.config['fork']:
             pid = os.fork()
 
         if pid:
@@ -21,25 +23,30 @@ class MakeMe(dict):
         raise NotImplementedError('check_messages not yet implemented')
 
     def running(self):
+        """Return True if the server is running, false otherwise."""
         return self._running
 
     def shutdown(self):
         raise NotImplementedError('server does not shutdown properly yet')
 
     def stop(self):
+        """Stop the server."""
         self._running = False
 
     def wait(self):
+        """Sleep until the message queue should be checked."""
         raise NotImplementedError('wait not yet implemented')
 
 
 class MakeMeCLI(MakeMe):
     def __init__(self):
+        """Load the MakeMe server and parse command line arguments."""
         MakeMe.__init__(self)
 
         self._parse_argv()
 
     def _parse_argv(self):
+        """Parse sys.argv for options, overloading those set in the config."""
         raise NotImplementedError('argv parsing not yet implemented')
 
 if __name__ == '__main__':
