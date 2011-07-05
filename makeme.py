fe9d50ceb73cbe22ef22197d8b1320c64367812b
Nathan Hoad <nathan@getoffmalawn.com>
2011-07-06 00:40:54 +1000

    Added a shell of a MailHandler class.

--- makeme.py
+++ makeme.py
@@ -5,6 +5,42 @@ import configparser
 import sys
 import time
 
+class MailHandler(object):
+    def __init__(self, username, password, smtp_server, smtp_port, imap_server, imap_port, use_ssl, use_tls):
+        """Set the username, password, SMTP and IMAP info, and log in.
+
+        Keyword arguments:
+        username -- username to log into IMAP and SMTP
+        password -- password to log into IMAP and SMTP
+        smtp_server -- IP or hostname of SMTP server
+        smtp_port -- port of SMTP server
+        imap_server -- IP or hostname of IMAP server
+        imap_port -- port of IMAP server
+        use_ssl -- boolean for whether to use ssl or not to connect to IMAP
+        use_tls -- boolean for whether to use tls or not to connect to SMTP
+
+        """
+        self.username = username
+        self.password = password
+        self.imap = (imap_server, imap_port, use_ssl)
+        self.smtp = (smtp_server, smtp_port, use_tls)
+
+        self._login_imap()
+        self._login_smtp()
+
+    def _login_imap(self):
+        """Log in to the IMAP server. Set self.imap to the connection object."""
+        pass
+
+    def _login_smtp(self):
+        """Log in to the SMTP server. Set self.smtp to the connection object."""
+        pass
+
+    def get_messages(self):
+        """Return a list of Email objects"""
+        return []
+
+
 class MakeMe(object):
     def __init__(self):
         """Initialise the MakeMe object. Sets _running to True, loads the config and logging, forking if requested."""
@@ -22,13 +58,13 @@ class MakeMe(object):
 
         if not self.config.getboolean('settings', 'sent_welcome_message'):
             self._send_welcome_message()
-            self.config.set('settings', True)
+            self.config.set('settings', 'sent_welcome_message', True)
 
             with open(self.local_config, 'w') as f:
                 self.config.write(f)
 
     def _act(self, message):
-        """Interpret and handle message.
+        """Interpret and handle a message.
 
         Keyword arguments:
         message -- Email object to parse and handle.
@@ -78,7 +114,27 @@ class MakeMe(object):
 
     def check_messages(self):
         """Check for messages and call _act() on each one."""
-        logging.info('check_message() not yet implemented')
+        logging.info('Checking messages...')
+
+        get = self.config.get
+        getint = self.config.getint
+        getboolean = self.config.getboolean
+        username = get('settings', 'username')
+        password = get('settings', 'password')
+        smtp_server = get('settings', 'smtp_server')
+        smtp_port = getint('settings', 'smtp_port')
+        imap_server = get('settings', 'imap_server')
+        imap_port = getint('settings', 'imap_port')
+        use_tls = getboolean('settings', 'smtp_tls')
+        use_ssl = getboolean('settings', 'imap_ssl')
+
+        messages = MailHandler(username, password, smtp_server, smtp_port, imap_server, imap_port, use_ssl, use_tls).get_messages()
+
+        if not messages:
+            logging.info('Received no messages')
+
+        for m in messages:
+            self._act(m)
 
     def running(self):
         """Return True if the server is running, false otherwise."""
@@ -122,3 +178,7 @@ try:
         server.shutdown()
 except KeyboardInterrupt:
     server.shutdown()
+except (configparser.NoSectionError, configparser.NoOptionError) as e:
+    print('Makeme: Error processing your makeme.conf:', e, file=sys.stderr)
+    server.shutdown()
+    sys.exit(1)
